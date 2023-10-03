b76211c013b2efc8fbf4a035de3274d3d99d26bc
Gustavo Eloi <gustavooeloi@gmail.com>
2023-10-03 19:47:49 -0300

    feature(navbar): adicionando funcionalidade de voltar o topo e fechar ao clicar nos link

--- src/app/core/components/header-navbar/header-navbar.component.ts
+++ src/app/core/components/header-navbar/header-navbar.component.ts
@@ -1,20 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header-navbar',
   templateUrl: './header-navbar.component.html',
   styleUrls: ['./header-navbar.component.css'],
 })
-export class HeaderNavbarComponent {
+export class HeaderNavbarComponent implements OnInit {
   imagePath = '../../components/../../../assets/images/oceb-logo-header.png';
 
   handleClick = false;
 
+  ngOnInit(): void {
+    document.addEventListener('click', () => {
+      this.closeMenu();
+    });
+  }
+
   addClickEvent() {
-    if (this.handleClick) {
-      this.handleClick = false;
-    } else {
-      this.handleClick = true;
-    }
+    this.handleClick = !this.handleClick;
+  }
+
+  closeMenu() {
+    this.handleClick = false;
+  }
+
+  handleClickMenu(event: Event) {
+    event.stopPropagation();
   }
 }
